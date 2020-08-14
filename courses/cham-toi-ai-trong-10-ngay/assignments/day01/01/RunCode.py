e1493a27694400036036bc698489fede5d3d64b6
ThachLN <ThachLN@gmail.com>
2020-08-14 18:06:35 +0700

    Bài tập ngày 1.

---
+++ courses/cham-toi-ai-trong-10-ngay/assignments/day01/01/RunCode.py
@@ -0,0 +1,12 @@
+# =============================================================================
+# Đây là hàm giới thiệu vài thông tin cá nhân. Trải nghiệm lệnh print.
+# =============================================================================
+def introduceMySelf():
+    name = 'Lê Ngọc Thạch'
+    print(name)
+    
+    email = 'LNThach@gmail.com'
+    print('Email:', email)
+    
+    phone = '09081234567'
+    print('Số {} là số điện thoại của tôi.'.format(phone))
